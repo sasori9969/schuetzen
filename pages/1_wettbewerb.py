b7519ff42af5e54e7aae9dfc452578314855d9b8
sasori9969 <64680421+sasori9969@users.noreply.github.com>
2025-01-29 10:10:25 +0000

    neu

--- pages/1_wettbewerb.py
+++ pages/1_wettbewerb.py
@@ -4,3 +4,14 @@ st.title("SSV 1928 e.V. Sulzbach")
 st.write(
     "Eingabe des Wettbewerbs"
 )
+
+# Eingabefelder
+wettbewerbsname = st.text_input("Wettbewerbsname")
+datum = st.date_input("Datum")
+teilnehmer = st.text_area("Teilnehmer (kommagetrennt)")
+disziplin = st.selectbox("Disziplin", ["Unterhebel", "KK", "Luftgewehr"])
+
+# Button zum Speichern
+if st.button("Wettbewerb speichern"):
+    # Hier würde der Code zum Speichern in der Datenbank stehen
+    st.success("Wettbewerb erfolgreich gespeichert!")
