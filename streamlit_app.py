8dc418d014277fd195144ab47318e270c894092a
sasori9969 <64680421+sasori9969@users.noreply.github.com>
2025-01-30 17:17:55 +0000

    a

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,3 +4,11 @@ st.title("SSV 1928 e.V. Sulzbach")
 st.write(
     "Software und Datenbank zur Verwaltung von Meisterschaften"
 )
+st.write("Erklärung und Ablauf: ")
+st.write("Links in der Navigation sind nacheinander die Optionen zu sehen.  ")
+st.write("Beginn: Wettbewerb. Hier wird der Wettbewerb angelegt. z.B. Ortsmeisterschaften 2025")
+st.write("Danach bei Schützen einen Teamnamen und/oder Schützen anlegen.")
+st.write("Unter Mannschaften können Mannschaften erstellt werden.")
+st.write("Bei Ergebnisse kann den Schützen das Schießergebnis zugeordnet werden.")
+st.write("Übersicht Mannschaft und Einzel zeigt die entsprechenden Ergebnisse an.")
+st.write("der Rest funktioniert wie es da steht ;-)")
